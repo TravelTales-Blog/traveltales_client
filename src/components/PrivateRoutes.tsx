d60d5a874cdc2537ef824b4d1a1db37fde720d70
Akila Budhvin Gamage <95086925+Akila-Gamage@users.noreply.github.com>
2025-05-11 00:30:59 +0530

    private routes added

---
+++ src/components/PrivateRoutes.tsx
@@ -0,0 +1,24 @@
+// src/components/PrivateRoute.tsx
+import React, { JSX } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+interface PrivateRouteProps {
+  children: JSX.Element;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
